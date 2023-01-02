6d9cf91738c41f2ebb50747c1bb48c666d9a2e30
h.young <cocotree221@gmail.com>
2023-01-02 15:38:08 +0900

    MongoDB, Express.js, Node.js 연동

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import NotFound from "./pages/NotFound";
+import Todo from "./pages/Todo";
+
+export default function App() {
+  return (
+    <Router>
+      <Header />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/todo" element={<Todo />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
---
+++ src/pages/Signup.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Signup = () => {
+  return <div className="p-6 m-4 shadow">Signup</div>;
+};
+
+export default Signup;
